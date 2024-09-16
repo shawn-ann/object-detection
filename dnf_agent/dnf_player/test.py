05da250e669d4dd6b5f4692242ca2e9af86d8d07
shawn <shawn_ann@126.com>
2024-09-16 19:14:46 +0800

    add

---
+++ dnf_agent/dnf_player/test.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("runs/detect/train3/weights/best.pt")  # pretrained YOLOv8n model
+
+# Run batched inference on a list of images
+results = model.predict(["img.jpg"],conf=0.40,iou=0.3)  # return a list of Results objects
+
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for cl  assification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
+    result.save(filename="result.jpg")  # save to disk
+
+
+
+# from roboflow import Roboflow
+# rf = Roboflow(api_key="JE2dF9UuZAK1IG3GtInx")
+# project = rf.workspace().project("dnf_agent")
+# model = project.version("1").model
+#
+# # infer on a local image
+# predict = model.predict("img.jpg", confidence=40, overlap=30).save("prediction.jpg")
---
+++ dnf_agent/dnf_player/train.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
+# Use the model
+model.train(data="../datasets/dnf_player_dataset_v1/data.yaml", epochs=3)  # train the model
+metrics = model.val()  # evaluate model performance on the validation set
+results = model("img.jpg")  # predict on an image
+# Process results list
+for result in results:
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    masks = result.masks  # Masks object for segmentation masks outputs
+    keypoints = result.keypoints  # Keypoints object for pose outputs
+    probs = result.probs  # Probs object for cl  assification outputs
+    obb = result.obb  # Oriented boxes object for OBB outputs
+    result.show()  # display to screen
+    result.save(filename="result.jpg")  # save to disk
+# path = model.export(format="onnx")  # export the model to ONNX format
